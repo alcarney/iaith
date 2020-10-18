0d1297e4e2850bbb5488b1245669cdf17af2b9f4
Alex Carney <alcarneyme@gmail.com>
2020-10-18 17:33:17 +0100

    Remove unecessary code

--- web/index.js
+++ web/index.js
@@ -5,25 +5,6 @@ const rust = import('./pkg');
 const runButton = document.getElementById("run")
 const input = document.getElementById("input")
 const output = document.getElementById("output")
-const tape = document.getElementById("tape")
-
-function setCell(index, value) {
-  let cell = tape.querySelector(`[data-index="${index}"]`)
-  if (!cell) {
-    cell = document.createElement("div")
-    cell.setAttribute("data-index", index)
-    cell.style.placeItems = "center"
-    cell.className = "grid flex-shrink-0 w-12 h-12 p-2 bg-white border"
-
-    tape.append(cell)
-  }
-
-  cell.innerText = `${value}`
-}
-
-for (let i = 0; i < 64; i++) {
-  //  setCell(i, 0)
-}
 
 rust
   .then(mod => {
